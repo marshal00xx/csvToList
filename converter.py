a58968612035a0145cde60a7d66b6637bf55048b
Aymane <81317119+Marshal099XXX@users.noreply.github.com>
2022-01-06 15:28:50 +0100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,11 @@
+import csv
+myList = ""
+counter = 0
+with open('Sheet1.csv') as csv_file:
+    # u can set the dilimiter you have in your csv file
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        if counter < 10:
+            myList += '\"' + str(row[0]) + '\", ' 
+file = open('result.txt', 'w')
+file.write(myList)
